deb4fdb5ac143bc6e867fd6138ad94bea6eddc52
millersy <millersy@seas.upenn.edu>
2019-09-04 15:34:09 -0400

    Deleting Files

--- Qt_GL_Test/.build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-######## DO NOT MODIFY ########################################################
-
-git submodule update --init
-mkdir -p build
-cd build
-qmake -Wall -spec linux-clang CONFIG+=address_sanitizer ..
-make --quiet
